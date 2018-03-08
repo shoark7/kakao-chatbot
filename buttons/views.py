11c1a77181b547fc880631db898e416fffe386be
Sunghwan Park <shoark7@gmail.com>
2018-03-08 14:31:13 +0900

    Create message api

--- buttons/views.py
+++ buttons/views.py
@@ -1,7 +1,29 @@
-from django.http import HttpResponse
+import json
+
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 
 def buttons(request):
-    return HttpResponse("hi!")
+    return JsonResponse({
+        'type': 'buttons',
+        'buttons': ['a', 'b', 'c', 'd'],
+    })
 
+
+@csrf_exempt
+def message(request):
+    json_str = request.body.decode('utf-8')
+    json_data = json.loads(json_str)
+    selected = json_data['content']
+
+    return JsonResponse({
+        "message": {
+            "text": "네 놈이 선택한 값은 {}입니다!!".format(selected)
+        },
+        "keyboard": {
+            "type": "buttons",
+            "buttons": ['a', 'b', 'c', 'd'],
+        }
+    })
